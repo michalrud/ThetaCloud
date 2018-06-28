23f432575e24fae43db81d094567d835f2b53bfa
Michał Rudowicz <michal.rudowicz@thaumatec.com>
2018-06-28 10:44:20 +0200

    I2CHelpers

---
+++ I2CHelpers.h
@@ -0,0 +1,49 @@
+#ifndef THETA_CLOUD_I2C_HELPERS_H
+#define THETA_CLOUD_I2C_HELPERS_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <array>
+#include <utility>
+
+const unsigned int READ_RETRIES = 10;
+
+class WriteToI2C
+{
+public:
+	WriteToI2C(uint8_t address)
+	{
+		Wire.beginTransmission(address);
+	}
+	template <typename T>
+	WriteToI2C& write(T data)
+	{
+		Wire.write(data);
+		return *this;
+	}
+	~WriteToI2C()
+	{
+		Wire.endTransmission();
+	}
+};
+
+template <uint8_t N>
+std::pair<std::array<uint8_t, N>, bool> ReadFromI2C(uint8_t address)
+{
+	Wire.requestFrom(address, N);
+	{
+		unsigned int attempt = 0;
+	    while (Wire.available() < N) {
+	        if (++attempt > READ_RETRIES) {
+	            return {std::array<uint8_t, N>(), true};	// error
+	        }
+	        delay(10);
+	    }
+	}
+	std::array<uint8_t, N> retval;
+	for (uint8_t i = 0; i < N; ++i)
+		retval[i] = Wire.read();
+	return {retval, false};
+}
+
+#endif  // THETA_CLOUD_I2C_HELPERS_H
